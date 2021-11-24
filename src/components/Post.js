8f3b31874200209500f75f8167cedbfb7bd024d3
Santos-Pazo <69361240+Santos-Pazo@users.noreply.github.com>
2021-11-24 14:33:45 -0300

    Correcciones

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,7 @@
 // Tarjeta de cada post
 
 import React, {Component} from 'react';
-import {Text, TouchableOpacity, View, ActivityIndicator, Image, FlatList, TextInput} from 'react-native';
+import {Text, TouchableOpacity, View, ActivityIndicator, Image, FlatList, Modal, TextInput} from 'react-native';
 import {StyleSheet} from 'react-native';
 import {auth, db} from '../firebase/config'
 import firebase from 'firebase';
